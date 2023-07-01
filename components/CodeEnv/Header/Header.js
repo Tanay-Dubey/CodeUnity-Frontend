29f39edb10f213d27243c9e2266b4517774b866b
Tanay Dubey <tanayspdubey@gmail.com>
2023-07-01 17:24:30 +0530

    Updated codeenv header

--- components/CodeEnv/Header/Header.js
+++ components/CodeEnv/Header/Header.js
@@ -1,11 +1,22 @@
 import styles from "/components/CodeEnv/Header/Header.module.css";
 import Image from "next/image";
 
-const Header=()=>{
-    return(
+const Header = () => {
+    return (
         <div className={styles.header}>
             <div className={styles.logo}>
-                <Image src={"/static/myprojects/logoname.png"} fill={true} ></Image>
+                <Image src={"/static/codeenv/logo.png"} fill={true} ></Image>
+            </div>
+            <div className={styles.projectName}>
+                Project Name
+            </div>
+            <div className={styles.allAccounts}>
+                <div className={styles.noOfOnlines}>
+                    6 Online
+                </div>
+                <div className={styles.account}>
+                    <span className={styles.accountLetter}>T</span>
+                </div>
             </div>
         </div>
     )
